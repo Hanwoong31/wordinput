518e1a09a18056f0d02abeb48f330100d1c3f44e
Hanwoong31 <108878740+Hanwoong31@users.noreply.github.com>
2025-06-25 12:04:17 +0900

    처음 커밋

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>단어 암기 앱</h1>
+    <WordInput />
+  </div>
+</template>
+
+<script>
+import WordInput from './components/WordInput.vue'
+
+export default {
+  components: {
+    WordInput
+  }
+}
+</script>
---
+++ src/components/WordInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input v-model="newWord" placeholder="단어 입력" />
+    <input v-model="newMeaning" placeholder="뜻 입력" />
+    <button @click="addWord">추가</button>
+
+    <ul>
+      <li v-for="(word, index) in words" :key="index">
+        {{ word.word }} - {{ word.meaning }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newWord: '',
+      newMeaning: '',
+      words: []
+    };
+  },
+  methods: {
+    addWord() {
+      this.words.push({ word: this.newWord, meaning: this.newMeaning });
+      this.newWord = '';
+      this.newMeaning = '';
+    }
+  }
+};
+</script>
